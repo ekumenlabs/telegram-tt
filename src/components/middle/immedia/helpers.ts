b4a30d1d77563ba32c93a7fa8503081c51f506a0
mattborghi <borghi.matias@gmail.com>
2022-01-03 17:23:08 -0300

    [EKUCODE] send an error message if the env variable is not set
    Signed-off-by: mattborghi <borghi.matias@gmail.com>

--- src/components/middle/immedia/helpers.ts
+++ src/components/middle/immedia/helpers.ts
@@ -1,3 +1,12 @@
 const formatRoom = (room: string) => room.replace('-', 's');
 
+const definedRoom = () => {
+  if (process.env.IMMEDIA_ROOM_ID === '') {
+    // eslint-disable-next-line no-console
+    console.error('IMMEDIA_ROOM_ID is not defined in the .env file.');
+  }
+};
+
+definedRoom();
+
 export { formatRoom };
